6dcb0b868c8f7bfbd0c59e5f76d07bb9fd3bb518
jeongpd <jeongpd@gmail.com>
2019-12-03 16:18:20 +0900

    Update config.py

--- conf/config.py
+++ conf/config.py
@@ -6,22 +6,22 @@ import os
 TZone = "Asia/Tokyo"
 
 # account
-ADMIN_ACCOUNT = "admin@lwbottest04"
-DOMAIN_ID = 153499
+ADMIN_ACCOUNT = "admin@alllogo.net"
+DOMAIN_ID = 140726
 
 # api
-API_ID = "kr1hLBbxHJiDs"
-SERVER_CONSUMER_KEY = "cqRT31EkE5TFcSZCkIzt"
+API_ID = "kr1pusdQpIZAD"
+SERVER_CONSUMER_KEY = "9GX72LcyjDB3PguhR1BK"
 
-SERVER_ID = "c2d6d1eb148b4144a9f75abe49faece0"
-SECRET_KEY_NAME = "private_20191127124751.key"
+SERVER_ID = "6353764416a444629f0545bb27c08cee"
+SECRET_KEY_NAME = "private_20191203160827.key"
 
 # LOCAL ADDRESS
-LOCAL_ADDRESS = "https://pm191127samplebot.herokuapp.com/"
+LOCAL_ADDRESS = "https://sample-bot-line-works.herokuapp.com/"
 
 # DB config
-DB_HOST = "ec2-54-235-248-185.compute-1.amazonaws.com"
-DB_NAME = "d948ducpbs8to4"
-DB_USER = "irxbslbpfagcyw"
+DB_HOST = "ec2-50-19-95-77.compute-1.amazonaws.com"
+DB_NAME = "d9815kls1qcsrq"
+DB_USER = "bxentvahhoezaz"
 DB_PORT = "5432"
-DB_PASSWORD = "4484cc1abc190bd1bc9c46da75ff2f67df260963ee3e87f0eb7fd2eecd3942d3"
+DB_PASSWORD = "a9f0305cfa4ebfd31b7e3c8f0cb762f0bdd36215d55c3b55cbb0f5a5f576e4ae"
